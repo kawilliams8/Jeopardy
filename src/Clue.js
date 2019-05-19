367fc68988a526ae8c28be1dcdab4bc7563017dc
Kayla <kayla.larson1990@gmail.com>
2019-05-19 14:32:16 -0600

    Add some functionality to Clue class and create tests for that
    co-authored-by: Colby Allen <colbyallen012@gmail.com>
    co-authored-by: Katherine Williams <kawilliams8@gmail.com>

--- src/Clue.js
+++ src/Clue.js
@@ -1,7 +1,36 @@
+import data from './data.js';
+
 class Clue {
   constructor() {
+    this.randomCategories = [];
+    this.usedCategories = [];
+    this.randomClues = [];
+  }
+
+  selectRandomCategories() {
+    this.randomCategories = Array(4).fill().map(() => Math.round(Math.random() * 10) + 1);
+    this.usedCategories.push(...this.randomCategories);
+  }
+
+  findCluesForACategory() {
+    let index = this.randomCategories[0];
+    let findClues = data.clues.filter(id => id.categoryId === index);
+    let randomClue100 = findClues.find(points => points.pointValue === 100);
+    let randomClue200 = findClues.find(points => points.pointValue === 200);
+    let randomClue300 = findClues.find(points => points.pointValue === 300);
+    let randomClue400 = findClues.find(points => points.pointValue === 400);
+    this.randomClues.push(randomClue100, randomClue200, randomClue300, randomClue400);
+  }
+  
+
+  populateBoard() {
+    //create method to randomize categoryID - 4 #s correlate to in the clues and pointValue
+  // then we need one random clue with each pointValue per category
+  //populateBoard
 
   }
+
+
 }
 
 export default Clue;
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,2 +1,9 @@
-import chai from 'chai'
-import Game from '../src/Game.js'
+import chai from 'chai';
+const expect = chai.expect;
+import Game from '../src/Game.js';
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
